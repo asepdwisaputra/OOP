c8631d271b30022f7c9498ab0d666a5f7e7d6469
Asep Dwi Saputra <127648406+asepdwisaputra@users.noreply.github.com>
2023-04-04 16:31:13 +0700

    Praktik OOP week 5

---
+++ Belajar_For_Loop.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variabel1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Belajar_For_Loop {
+    public static void main(String[] args) {
+        
+    for(int a=1; a<=20; a++){
+            System.out.println(a);
+        }
+        
+    }
+}
